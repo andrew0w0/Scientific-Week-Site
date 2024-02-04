10094655f77680111687e3900fc23a8e6748ee32
andrew0w0 <90263133+andrew0w0@users.noreply.github.com>
2024-02-04 19:46:27 +0100

    Added Firebase and some css changes

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBPw6e5AHY5LqQavX-87msQabKTFX-mHcs",
+  authDomain: "scientific-week.firebaseapp.com",
+  projectId: "scientific-week",
+  storageBucket: "scientific-week.appspot.com",
+  messagingSenderId: "222879210997",
+  appId: "1:222879210997:web:ff670eebb1f7628c2794bc",
+  measurementId: "G-WZV0HEYPCS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
 const button = document.getElementById("generate-button")
 
 button.addEventListener("click", (MouseEvent) => {
-  
+  window.location.href = "./src/pages/input/input.html"
 })
